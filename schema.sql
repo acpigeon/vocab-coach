19bfdeffc2df755a4f3bbe84096d99fee6ef6f60
Aaron Pigeon <acpigeon@gmail.com>
2013-03-23 10:10:40 -0500

    added readme

--- schema.sql
+++ schema.sql
@@ -1,35 +1,4 @@
-CREATE TABLE `tblVocab` (
-	`vocab_id` INTEGER NOT NULL,
-	`canonical_id` INTEGER NOT NULL,
-	`vocabulary` TEXT NOT NULL,
-	`part_of_speech` TEXT NOT NULL,
-	`type` TEXT NOT NULL,
-	`gender` TEXT NOT NULL,
-	`language` TEXT NOT NULL,
-	PRIMARY KEY (`vocab_id`)
-)
-
-CREATE TABLE `tblRoot` (
-	`canonical_id` INTEGER NOT NULL,
-	`root` TEXT NOT NULL,
-	PRIMARY KEY (`canonical_id`)
-)
-
-CREATE TABLE `tblContext` (
-	`context_id` INTEGER NOT NULL,
-	`context` TEXT NOT NULL,
-	PRIMARY KEY (`context_id`)
-)
-
-CREATE TABLE `tblTags` (
-	`tag_id` INTEGER NOT NULL,
-	`tag` TEXT NOT NULL,
-	PRIMARY KEY (`tag_id`)
-)
-
-CREATE TABLE `tblRecall` (
-	`recall_id` INTEGER NOT NULL,
-	`vocab_id` INTEGER NOT NULL,
-	`date` TEXT NOT NULL,
-	PRIMARY KEY (`recall_id`)
-)
+CREATE TABLE tblContext(context_id INTEGER PRIMARY KEY, context TEXT, vocab_id INTEGER);
+CREATE TABLE tblRoot(canonical_id INTEGER PRIMARY KEY, root TEXT);
+CREATE TABLE tblTags(tag_id INTEGER PRIMARY KEY, tag TEXT, vocab_id INTEGER);
+CREATE TABLE tblVocab(vocab_id INTEGER PRIMARY KEY, canonical_id INTEGER, vocabulary TEXT, part_of_speech TEXT, type TEXT, gender TEXT, language TEXT);
--- vocab.py
+++ vocab.py
@@ -4,8 +4,48 @@ import os
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 DATABASE = os.path.join(PROJECT_ROOT, 'tmp', 'vocab.db')
 
-con = sql.connect(DATABASE)
-with con:
-	cur = con.cursor()
-	#cur.execute("CREATE TABLE tblVocab(vocab_id INT, canonical_id INT, vocabulary TEXT, part_of_speech TEXT, type TEXT, gender TEXT, language TEXT)")
-	cur.execute("INSERT INTO tblVocab VALUES(?,?,?,?,?,?,?)", (1, 1, 'tener', 'verb', 'infinitive', 'nothing', 'spanish'))
+# sample entry
+wordAttributes = {'vocab': 'tener', 'canonical': 'tener', 'partOfSpeech': 'verb', 'type': 'infinitive', 'gender': 'NULL', 'language': 'spanish',
+    'contexts': ['er verbs', 'exam 1', 'exam 2'],
+    'tags': ['memory aid', 'whatever else']
+}
+
+def createNewVocab (wordAttributes):
+
+    con = sql.connect(DATABASE)
+    with con:
+        cur = con.cursor()
+
+        #cur.execute("DROP TABLE IF EXISTS tblVocab")
+        #cur.execute("DROP TABLE IF EXISTS tblRoot")
+        #cur.execute("DROP TABLE IF EXISTS tblContext")
+        #cur.execute("DROP TABLE IF EXISTS tblTags")
+
+        #cur.execute("CREATE TABLE tblVocab(vocab_id INTEGER, canonical_id INTEGER, vocabulary TEXT, part_of_speech TEXT, type TEXT, gender TEXT, language TEXT)")
+        #cur.execute("CREATE TABLE tblRoot(canonical_id INTEGER, root TEXT)")
+        #cur.execute("CREATE TABLE tblContext(context_id INTEGER, context TEXT, vocab_id INTEGER)")
+        #cur.execute("CREATE TABLE tblTags(tag_id INTEGER, tag TEXT, vocab_id INTEGER)")
+
+        #tblVocabIndex = 0
+        #tblRootIndex = 0
+        #tblContextIndex = 0
+        #tblTagsIndex = 0
+
+        #print type(cur.execute("SELECT COUNT(`vocab_id`) FROM tblVocab")) # <type 'sqlite3.Cursor'>
+        #print cur.execute("SELECT COUNT(`vocab_id`) FROM tblVocab") # <sqlite3.Cursor object at 0x100486420>
+
+        tblVocabIndex = cur.execute("SELECT COUNT(`vocab_id`) FROM tblVocab").fetchone()[0] - 1
+        tblRootIndex = cur.execute("SELECT COUNT(`canonical_id`) FROM tblRoot").fetchone()[0] - 1
+        tblContextIndex = cur.execute("SELECT COUNT(`context_id`) FROM tblContext").fetchone()[0] - 1
+        tblTagsIndex = cur.execute("SELECT COUNT(`tag_id`) FROM tblTags").fetchone()[0] - 1
+
+        cur.execute("INSERT INTO tblVocab VALUES(?,?,?,?,?,?,?)", (tblVocabIndex, tblRootIndex, wordAttributes['vocab'], wordAttributes['partOfSpeech'], wordAttributes['type'], wordAttributes['gender'], wordAttributes['language']))
+        cur.execute("INSERT INTO tblRoot VALUES(?,?)", (tblRootIndex, wordAttributes['canonical']))
+        for context in wordAttributes['contexts']:
+            cur.execute("INSERT INTO tblContext VALUES(?,?,?)", (tblContextIndex, context, tblVocabIndex))
+        for tag in wordAttributes['tags']:
+            cur.execute("INSERT INTO tblTags VALUES(?,?,?)", (tblTagsIndex, tag, tblVocabIndex))
+
+
+
+createNewVocab(wordAttributes)
